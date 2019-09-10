3cd17dbf0e4d4e3478b260f9fcc75a2ad3d1ecf5
Stas Kozar <stas.kozar@transoftgroup.com>
2019-09-10 14:19:04 +0300

    Hometask 1.2: Soft Dependency - di.xml (part 1)

---
+++ app/code/Training/TestOM/Model/Manager.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace Training\TestOM\Model;
+
+class Manager implements ManagerInterface
+{
+
+    public function create()
+    {
+        // TODO: Implement create() method.
+    }
+
+    public function get()
+    {
+        // TODO: Implement get() method.
+    }
+}
---
+++ app/code/Training/TestOM/Model/ManagerInterface.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+namespace Training\TestOM\Model;
+
+interface ManagerInterface
+{
+    public function create();
+
+    public function get();
+}
