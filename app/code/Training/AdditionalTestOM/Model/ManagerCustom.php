b455af5d3fa2d5a539f4b7cfefe3bace9371d0c8
Stas Kozar <stas.kozar@transoftgroup.com>
2019-09-16 17:15:04 +0300

    Hometask 1.3: di.xml (part 2)

---
+++ app/code/Training/AdditionalTestOM/Model/ManagerCustom.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace Training\AdditionalTestOM\Model;
+
+use Training\TestOM\Model\ManagerInterface;
+
+class ManagerCustom implements ManagerInterface
+{
+
+    public function create()
+    {
+        // TODO: Implement create() method.
+    }
+
+    public function get()
+    {
+        // TODO: Implement get() method.
+    }
+}
