a11bc2606a971b8afcf15474aebae8394b96a830
Stas Kozar <stas.kozar@transoftgroup.com>
2019-09-23 12:48:02 +0300

    Hometask 2.1: Log page response observer

---
+++ app/code/Training/Test/Observer/LogPageHtml.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+
+namespace Training\Test\Observer;
+
+use Magento\Framework\Event\Observer;
+use Magento\Framework\Event\ObserverInterface;
+use Psr\Log\LoggerInterface;
+
+class LogPageHtml implements ObserverInterface
+{
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    /**
+     * @param Observer $observer
+     * @return void
+     */
+    public function execute(Observer $observer)
+    {
+        $response = $observer->getEvent()->getData('response');
+
+        $this->logger->debug($response->getBody());
+    }
+}
