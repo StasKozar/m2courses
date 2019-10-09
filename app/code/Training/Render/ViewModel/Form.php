32fa2df5a8785e2459d745a71d48f67d3e327f66
Stas Kozar <stas.kozar@transoftgroup.com>
2019-10-09 13:05:54 +0300

    Hometask 3.14: Add form to page with ViewModel

---
+++ app/code/Training/Render/ViewModel/Form.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+namespace Training\Render\ViewModel;
+
+use Magento\Framework\View\Element\Block\ArgumentInterface;
+
+class Form implements ArgumentInterface
+{
+    /**
+     * @var \Magento\Framework\UrlInterface
+     */
+    private $urlBuilder;
+
+    /**
+     * @param \Magento\Framework\UrlInterface $urlBuilder
+     */
+    public function __construct(
+        \Magento\Framework\UrlInterface $urlBuilder
+    ) {
+        $this->urlBuilder = $urlBuilder;
+    }
+
+    public function getSubmitUrl()
+    {
+        return $this->urlBuilder->getUrl('customer/account/login');
+    }
+}
